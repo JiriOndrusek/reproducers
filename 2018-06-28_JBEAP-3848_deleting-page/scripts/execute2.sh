c68c55c2c68393f8946b423a5990bc60ad1f4bf4
JiriOndrusek <jondruse@redhat.com>
2018-06-29 14:39:07 +0200

    2018-06-28_JBEAP-3848_deleting-page

---
+++ 2018-06-28_JBEAP-3848_deleting-page/scripts/execute2.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+./delete.sh
+
+./prepare.sh
+
+java -jar ../client/target/JBEAP-3848_deleting-page-client-1.0.0-SNAPSHOT.jar
+
+java -cp ../client/target/JBEAP-3848_deleting-page-client-1.0.0-SNAPSHOT.jar jondruse.ClientReceiver
+
+./kill.sh
+
+rm -rf ${EAP_HOME}-nodes-server/data/activemq/paging
+
+./run.sh
+
+sleep 4s
+
+java -jar ../client/target/JBEAP-3848_deleting-page-client-1.0.0-SNAPSHOT.jar
+
+java -cp ../client/target/JBEAP-3848_deleting-page-client-1.0.0-SNAPSHOT.jar jondruse.ClientReceiver
